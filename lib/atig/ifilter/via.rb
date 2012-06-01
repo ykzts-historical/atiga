da527f27e6582fd92b4faf467b374aecf90aaa29
Tomonori Tamagawa <xtuaok@gmail.com>
2012-06-01 15:20:58 +0900

    DM には source がないらしいのでそのまま return

--- lib/atig/ifilter/via.rb
+++ lib/atig/ifilter/via.rb
@@ -3,6 +3,7 @@ module Atig
   module IFilter
     module Via
       def self.call(status)
+        return status unless status.source
         source = status.source.gsub(/<[^>]+>/, '')
         status.merge :text => "#{status.text} via #{source}"
       end
